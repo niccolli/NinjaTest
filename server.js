7723b2690566947d30c6bb1fa5c629cdfe5125b1
Yusuke Umahashi <Yusuke.Umahashi@gmail.com>
2012-03-18 15:40:29 +0900

    add views

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 var util = require('util');
 var ws  = require('websocket-server');
+var express = require('express');
 
-var sockets = [];
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler()); 
+});
+
+/*app.get('/', function(req, res){
+	res.render('index', {
+		title: 'Express'
+	});
+});*/
+app.listen(3000);
+
+
 var streamFromID = null;
 
 var server = ws.createServer();
